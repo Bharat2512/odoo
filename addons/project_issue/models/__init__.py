388d6681dbb4cb3091bb1e39cd86505e59c13257
vbh-odoo <vbh@odoo.com>
2015-07-27 13:00:08 +0530

    [MOV] project_issue: split project_issue.py and project_issue_view files

--- addons/project_issue/models/__init__.py
+++ addons/project_issue/models/__init__.py
@@ -2,4 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 import project_issue
+import account_analytic_account
+import project_project
 import project_task_type
+import res_partner
