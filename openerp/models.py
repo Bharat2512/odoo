767312d6ca21ade4a5815857ddfb36959c4afcfa
Raphael Collet <rco@openerp.com>
2016-05-25 15:54:38 +0200

    BaseModel: convert method copy_data()

--- openerp/models.py
+++ openerp/models.py
@@ -4752,91 +4752,71 @@ class BaseModel(object):
         else:
             return ffield.column.search(self._cr, self._model, args or [], field, value, offset, limit, self._uid)
 
-    def copy_data(self, cr, uid, id, default=None, context=None):
+    @api.multi
+    def copy_data(self, default=None):
         """
         Copy given record's data with all its fields values
 
-        :param cr: database cursor
-        :param uid: current user id
-        :param id: id of the record to copy
         :param default: field values to override in the original values of the copied record
-        :type default: dictionary
-        :param context: context arguments, like lang, time zone
-        :type context: dictionary
         :return: dictionary containing all the field values
         """
-
-        if context is None:
-            context = {}
-
         # avoid recursion through already copied records in case of circular relationship
-        if '__copy_data_seen' not in context:
-            context = dict(context, __copy_data_seen=defaultdict(list))
-        seen_map = context['__copy_data_seen']
-        if id in seen_map[self._name]:
+        if '__copy_data_seen' not in self._context:
+            self = self.with_context(__copy_data_seen=defaultdict(set))
+        seen_map = self._context['__copy_data_seen']
+        if self.id in seen_map[self._name]:
             return
-        seen_map[self._name].append(id)
+        seen_map[self._name].add(self.id)
 
-        if default is None:
-            default = {}
-        if 'state' not in default:
-            if 'state' in self._defaults:
-                if callable(self._defaults['state']):
-                    default['state'] = self._defaults['state'](self, cr, uid, context)
-                else:
-                    default['state'] = self._defaults['state']
+        default = dict(default or [])
+        if 'state' not in default and 'state' in self._fields:
+            field = self._fields['state']
+            if field.default:
+                value = field.default(self)
+                value = field.convert_to_cache(value, self)
+                value = field.convert_to_record(value, self)
+                value = field.convert_to_write(value, self)
+                default['state'] = value
 
         # build a black list of fields that should not be copied
         blacklist = set(MAGIC_COLUMNS + ['parent_left', 'parent_right'])
         whitelist = set(name for name, field in self._fields.iteritems() if not field.inherited)
 
-        def blacklist_given_fields(obj):
+        def blacklist_given_fields(model):
             # blacklist the fields that are given by inheritance
-            for other, field_to_other in obj._inherits.items():
-                blacklist.add(field_to_other)
-                if field_to_other in default:
-                    # all the fields of 'other' are given by the record: default[field_to_other],
-                    # except the ones redefined in self
-                    blacklist.update(set(self.pool[other]._fields) - whitelist)
+            for parent_model, parent_field in model._inherits.items():
+                blacklist.add(parent_field)
+                if parent_field in default:
+                    # all the fields of 'parent_model' are given by the record:
+                    # default[parent_field], except the ones redefined in self
+                    blacklist.update(set(self.env[parent_model]._fields) - whitelist)
                 else:
-                    blacklist_given_fields(self.pool[other])
+                    blacklist_given_fields(self.env[parent_model])
             # blacklist deprecated fields
-            for name, field in obj._fields.iteritems():
+            for name, field in model._fields.iteritems():
                 if field.deprecated:
                     blacklist.add(name)
 
         blacklist_given_fields(self)
 
+        fields_to_copy = {name: field
+                          for name, field in self._fields.iteritems()
+                          if field.copy and name not in default and name not in blacklist}
 
-        fields_to_copy = dict((f,fi) for f, fi in self._fields.iteritems()
-                                     if fi.copy
-                                     if f not in default
-                                     if f not in blacklist)
-
-        data = self.read(cr, uid, [id], fields_to_copy.keys(), context=context)
-        if data:
-            data = data[0]
-        else:
-            raise IndexError(_("Record #%d of %s not found, cannot copy!") % ( id, self._name))
-
-        res = dict(default)
-        for f, field in fields_to_copy.iteritems():
-            if field.type == 'many2one':
-                res[f] = data[f] and data[f][0]
-            elif field.type == 'one2many':
-                other = self.pool[field.comodel_name]
+        for name, field in fields_to_copy.iteritems():
+            if field.type == 'one2many':
                 # duplicate following the order of the ids because we'll rely on
                 # it later for copying translations in copy_translation()!
-                lines = [other.copy_data(cr, uid, line_id, context=context) for line_id in sorted(data[f])]
+                lines = [rec.copy_data() for rec in self[name].sorted(key='id')]
                 # the lines are duplicated using the wrong (old) parent, but then
                 # are reassigned to the correct one thanks to the (0, 0, ...)
-                res[f] = [(0, 0, line) for line in lines if line]
+                default[name] = [(0, 0, line) for line in lines if line]
             elif field.type == 'many2many':
-                res[f] = [(6, 0, data[f])]
+                default[name] = [(6, 0, self[name].ids)]
             else:
-                res[f] = data[f]
+                default[name] = field.convert_to_write(self[name], self)
 
-        return res
+        return default
 
     def copy_translations(self, cr, uid, old_id, new_id, context=None):
         if context is None:
