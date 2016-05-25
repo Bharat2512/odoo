9e138798a5f20500897daf869b4e3439334809d0
Raphael Collet <rco@openerp.com>
2016-05-25 13:59:09 +0200

    BaseModel: convert method _generate_translated_field()

--- openerp/models.py
+++ openerp/models.py
@@ -4572,27 +4572,23 @@ class BaseModel(object):
 
         :return: the qualified field name (or expression) to use for ``field``
         """
-        lang = self._context.get('lang')
-        if lang:
+        if self.env.lang:
             # Sub-select to return at most one translation per record.
             # Even if it shoud probably not be the case,
             # this is possible to have multiple translations for a same record in the same language.
             # The parenthesis surrounding the select are important, as this is a sub-select.
             # The quotes surrounding `ir_translation` are important as well.
             unique_translation_subselect = """
-            (SELECT DISTINCT ON (res_id) res_id, value
-            FROM "ir_translation"
-            WHERE
-                name = %s AND
-                lang = %s AND
-                value != %s
-            ORDER BY res_id, id DESC)
+                (SELECT DISTINCT ON (res_id) res_id, value
+                 FROM "ir_translation"
+                 WHERE name=%s AND lang=%s AND value!=%s
+                 ORDER BY res_id, id DESC)
             """
             alias, alias_statement = query.add_join(
                 (table_alias, unique_translation_subselect, 'id', 'res_id', field),
                 implicit=False,
                 outer=True,
-                extra_params=["%s,%s" % (self._name, field), lang, ""],
+                extra_params=["%s,%s" % (self._name, field), self.env.lang, ""],
             )
             return 'COALESCE("%s"."%s", "%s"."%s")' % (alias, 'value', table_alias, field)
         else:
