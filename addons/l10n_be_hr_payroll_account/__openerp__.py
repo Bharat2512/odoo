56e2c44d8c7cac4b6e7969de4b1d9555e0016975
Rohan Patel <rop@odoo.com>
2016-05-27 16:20:41 +0530

    [MIG] l10n_be_hr_payroll_account : Migrated to new API.

--- addons/l10n_be_hr_payroll_account/__openerp__.py
+++ addons/l10n_be_hr_payroll_account/__openerp__.py
@@ -1,5 +1,6 @@
 # -*- encoding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': 'Belgium - Payroll with Accounting',
     'category': 'Localization',
@@ -12,10 +13,8 @@ Accounting Data for Belgian Payroll Rules.
 
     'auto_install': True,
     'website': 'https://www.odoo.com/page/accounting',
-    'demo': [],
     'data':[
         'data/l10n_be_hr_payroll_account_data.xml',
     ],
     'post_init_hook': '_set_accounts',
-    'installable': True
 }
