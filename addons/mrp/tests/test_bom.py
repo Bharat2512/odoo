afb6b24630cc85c3660edec9c342f0fac9f4b201
Gaurav Panchal <gan@openerp.com>
2016-06-07 15:12:27 +0530

    [IMP] mrp : Improve mrp testcase with phantom bom

--- addons/mrp/tests/test_bom.py
+++ addons/mrp/tests/test_bom.py
@@ -28,36 +28,57 @@ class TestBoM(TestMrpCommon):
         # print res
 
     def test_explode_from_order(self):
-        # reset quantities
+        """ Testing production orders with phantom bom .."""
+
+        # Update capacity, start time, stop time, and time efficiency.
+        # ------------------------------------------------------------
+        self.workcenter_1.write({'capacity': 1, 'time_start': 0, 'time_stop': 0, 'time_efficiency': 100})
+
+        # Set manual time cycle 20 and 10.
+        # --------------------------------
+        self.operation_1.write({'time_cycle_manual': 20})
+        self.operation_2.write({'time_cycle_manual': 10})
+
+        # Set routing false on phantom BOM.
+        # ---------------------------------
+        self.bom_2.write({'routing_id': False})
+
+
+        # Reset quantities
+        # ----------------
         self.env['stock.change.product.qty'].create({
             'product_id': self.product_1.id,
             'new_quantity': 0.0,
             'location_id': self.warehouse_1.lot_stock_id.id,
         }).change_product_qty()
 
+        # Set the tracking by lot for product Batten and Shelf Bracket
+        # ------------------------------------------------------------
         (self.product_2 | self.product_4).write({
             'tracking': 'lot',
         })
-        # assign consume material
+        # assign consume material and check availability.
+        # -----------------------------------------------
         self.production_1.action_assign()
-        self.assertEqual(self.production_1.availability, 'waiting')
+        self.assertEqual(self.production_1.availability, 'waiting', "Production order should be in waiting state.")
 
-        # check consume materials of manufacturing order
-        self.assertEqual(len(self.production_1.move_raw_ids), 3)
-        # for move in self.production_1.move_raw_ids:
-        #     print move.name, move.state, move.product_id, move.product_qty, move.product_uom_qty, move.unit_factor
+        # Check consume material of production order.
+        # -------------------------------------------
+        self.assertEqual(len(self.production_1.move_raw_ids), 3, "Consume material lines are not generated proper.")
         product_2_consume_moves = self.production_1.move_raw_ids.filtered(lambda x: x.product_id == self.product_2)
         product_4_consume_moves = self.production_1.move_raw_ids.filtered(lambda x: x.product_id == self.product_4)
         product_5_consume_moves = self.production_1.move_raw_ids.filtered(lambda x: x.product_id == self.product_5)
-        self.assertEqual(product_2_consume_moves.product_uom_qty, 2.0)
-        self.assertEqual(product_4_consume_moves.product_uom_qty, 6.0)
-        self.assertEqual(product_5_consume_moves.product_uom_qty, 3.0)
+        self.assertEqual(product_2_consume_moves.product_uom_qty, 2.0, "Wrong consume quantity of product Batten.")
+        self.assertEqual(product_4_consume_moves.product_uom_qty, 6.0, "Wrong consume quantity of product Shelf Bracket.")
+        self.assertEqual(product_5_consume_moves.product_uom_qty, 3.0, 'Wrong consume quantity of product Rafter.')
 
-        # create required lots
+        # Create Lots for product Batten and Shelf Bracket.
+        # -------------------------------------------------
         lot_product_2 = self.env['stock.production.lot'].create({'product_id': self.product_2.id})
         lot_product_4 = self.env['stock.production.lot'].create({'product_id': self.product_4.id})
 
-        # refuel stock
+        # Create Inventory for consume material product Batten, Shelf Bracket and Rafter.
+        # -------------------------------------------------------------------------------
         inventory = self.env['stock.inventory'].create({
             'name': 'Inventory For Product C',
             'filter': 'partial',
@@ -83,34 +104,36 @@ class TestBoM(TestMrpCommon):
         inventory.prepare_inventory()
         inventory.action_done()
 
-        # re-assign consume material
+        # Re-assign consume material and check production order status after assign.
+        # -------------------------------------------------------------------------
         self.production_1.action_assign()
+        self.assertEqual(self.production_1.availability, 'assigned', "Production order should be in assigned state.")
 
-        # Check production order status after assign.
-        self.assertEqual(self.production_1.availability, 'assigned')
-        # Plan production order.
+        # Plan for production order.
+        # --------------------------
         self.production_1.button_plan()
 
-        # check workorders
+        # ---------------------------------------------
+        # Check workorder process of production order.
+        # ---------------------------------------------
 
         workorders = self.production_1.workorder_ids
-        # for workorder in workorders:
-        #     print workorder.name, workorder.product_id, workorder.qty_producing
 
-        # first machine (machine A)
-        # self.assertEqual(workorders[0].duration, 40)
+        # Check machine A process....
+        # ---------------------------
+        self.assertEqual(workorders[0].duration_expected, 40, "Workorder duration does not match.")
         workorders[0].button_start()
         finished_lot = self.env['stock.production.lot'].create({'product_id': self.production_1.product_id.id})
         workorders[0].write({'final_lot_id': finished_lot.id, 'qty_producing': 48})
-
         product_d_move_lot = workorders[0].active_move_lot_ids.filtered(lambda x: x.product_id == self.product_2)
         product_d_move_lot.write({'lot_id': lot_product_2.id, 'quantity_done': 2})
         workorders[0].record_production()
 
         # Check machine B process....
-        # self.assertEqual(workorders[1].duration, 20, "Workorder duration does not match.")
+        # ---------------------------
+        self.assertEqual(workorders[1].duration_expected, 20, "Workorder duration does not match.")
         workorders[1].button_start()
-        product_f_move_lot = workorders[1].active_move_lot_ids.filtered(lambda x: x.product_id == self.product_5)
+        product_f_move_lot = workorders[1].active_move_lot_ids.filtered(lambda x: x.product_id == self.product_4)
         product_f_move_lot.write({'lot_id': lot_product_4.id, 'quantity_done': 6})
         workorders[1].record_production()
         self.production_1.button_mark_done()
