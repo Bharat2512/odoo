ccdd30285766a4e5bfaa655881b87ed22fdccb54
Rohan Patel <rop@odoo.com>
2016-06-08 18:16:32 +0530

    [MIG] l10n_in_hr_payroll : Migrated to new API.

--- addons/l10n_in_hr_payroll/__openerp__.py
+++ addons/l10n_in_hr_payroll/__openerp__.py
@@ -1,11 +1,10 @@
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+
 {
     'name': 'Indian Payroll',
     'category': 'Localization',
-    'website':'http://www.openerp.com',
     'depends': ['hr_payroll'],
-    'version': '1.0',
     'description': """
 Indian Payroll Salary Rules.
 ============================
@@ -43,7 +42,5 @@ Indian Payroll Salary Rules.
         'test/payment_advice.yml',
         'test/payment_advice_batch.yml'
     ],
-
     'demo': ['demo/l10n_in_hr_payroll_demo.xml'],
-    'installable': True
 }
