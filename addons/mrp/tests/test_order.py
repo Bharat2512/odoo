08f205769fb6a67cd3d4c9cd9ec647502da401e9
ptr <ptr@openerp.com>
2016-06-09 15:22:36 +0530

    [ADD] mrp: add new test case for check production order availability is partially_available.

--- addons/mrp/tests/test_order.py
+++ addons/mrp/tests/test_order.py
@@ -25,3 +25,45 @@ class TestMrpOrder(TestMrpCommon):
     def test_flow(self):
         production = self.production_1.sudo(self.user_mrp_user)
         # production.action_compute()
+
+    def test_production_avialability(self):
+        """
+            Test availability of production order.
+        """
+        self.bom_2.write({'routing_id': False, 'type': 'normal'})
+
+        production_2 = self.env['mrp.production'].create({
+            'name': 'MO-Test001',
+            'product_id': self.product_3.id,
+            'product_qty': 20,
+            'bom_id': self.bom_2.id,
+            'product_uom_id': self.product_1.uom_id.id,
+        })
+        production_2.action_assign()
+
+        # check sub product availability state is waiting
+        self.assertEqual(production_2.availability, 'waiting', 'Production order should be availability for waiting state')
+
+        # Update Inventory
+        inventory_wizard = self.env['stock.change.product.qty'].create({
+            'product_id': self.product_4.id,
+            'new_quantity': 10.0,
+        })
+        inventory_wizard.change_product_qty()
+
+        production_2.action_assign()
+
+        # check sub product availability state is partially available
+        self.assertEqual(production_2.availability, 'partially_available', 'Production order should be availability for partially available state')
+
+        # Update Inventory
+        inventory_wizard = self.env['stock.change.product.qty'].create({
+            'product_id': self.product_4.id,
+            'new_quantity': 30.0,
+        })
+        inventory_wizard.change_product_qty()
+
+        production_2.action_assign()
+
+        # check sub product availability state is assigned
+        self.assertEqual(production_2.availability, 'assigned', 'Production order should be availability for assigned state')
